09e5879d4670936d5a0a4cfd075f01062c9bf4d6
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2024-12-11 15:40:49 +1100

    crate custom hook for fetching games

--- game-hub/src/services/api-client.ts
+++ game-hub/src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "cad57ed09a394e948546b9e5efe8dc44",
+    key: "cad57ed09a394e948546b9e5efe8dc44", //not secure, normally would be stored on backend server
   },
 });
