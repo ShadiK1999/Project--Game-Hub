9f65631c301df3e961487202b5123b707f7e283e
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2024-12-13 11:27:54 +1100

    ship genres with the app

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import { HStack, Image } from "@chakra-ui/react";
 import React from "react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
-import SearchInput from "./searchInput";
+import SearchInput from "./SearchInput";
 interface Props {
   onSearch: (searchText: string) => void;
 }
